232a424ef0c9b9b1886c1cf1c483ec12f71045fc
RossohinPavel <rossohinpavel@yandex.ru>
2022-10-18 21:53:14 +0300

    написана итоговая работа на множестав

--- test.py
+++
@@ -1,8 +0,0 @@
-sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a 
-              pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, 
-              over which, if you can still stand my style (I am writing under observation), the sun of my infancy had 
-              set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge 
-              in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; 
-              a furry warmth, golden midges.'''
-myset = {x.lower().strip('(,):;.') for x in sentence.split() if len(x.lower().strip('(,):;.')) < 4}
-print(*sorted(myset))
